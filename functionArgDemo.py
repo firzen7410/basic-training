90f2d4b2b2b76edc45a59bf0f03621346368bc01
ELAND\trjhang <trjhong@eland.com.tw>
2025-08-13 17:11:31 +0800

    sourceTreeCommitTest
    Signed-off-by: ELAND\trjhang <trjhong@eland.com.tw>

--- functionArgDemo.py
+++ functionArgDemo.py
@@ -1,18 +1,39 @@
-def double(*args):
-    return [x*2 for x in args ]
-print(double(2,6,7,4,2))
+def score(eg_score, math_score, el_weight, math_weight):
+    return eg_score * el_weight + math_score * math_weight
 
-def find(key,**kwarg):
-    return kwarg[key] if key in kwarg else None
 
-def connect(**kwargs):
-    host = kwargs.get('host', 'localhost')
-    port = kwargs.get('port', 3306)
-    user = kwargs.get('user', 'root')
-    print(f"連線到 {host}:{port} 使用者 {user}")
+# positional argument
+print(score(70, 60, 0.5, 0.5))
 
-connect()
-connect(host='192.168.1.1', user='admin')
+# keyword argument
+print(score(math_score=70, el_weight=0.5, math_weight=0.5, eg_score=60))
 
-params = {'x': 10, 'y': 20, 'z': 30}
-print(find('y',**params))
+def fun(a, b, c=10, d=20):
+    return a + b + c + d
+print(fun(3, 4))
+
+# 必要參數要放在有預設值參數的前面
+# def fun(a=10, b=20, c, d) >> syntax error
+
+# 位置參數必須放在關鍵字參數前面
+# print(fun(a=10,b=20,30,40)) >> syntax error
+
+# unpacking參數 * 與 **
+#傳入時拆包
+a = [2,3]
+b = {'c':4,'d':5}
+print(fun(*a,**b))
+
+# 接收時拆包，函式在接收時會打包成tuple
+def sum_numbers(*args):
+    print(type(args))
+    total = 0
+    for num in args:
+        total += num
+    return total
+print(sum_numbers(1,2,3))
+
+def show(**args):
+    for key, value in args.items():
+        print(key, value)
+show(name='jack',age='29')
